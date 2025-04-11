aeaa4b3b6316de914c5fc6911d510a828ad93c1d
Oscar <oscarcookeabbott@gmail.com>
2025-04-11 16:19:50 +1000

    Attempt to fix Pages asset URLs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite"
 import tailwindcss from "@tailwindcss/vite"
 
-export default defineConfig({
-    root: "./src/docs",
-    plugins: [tailwindcss()],
+export default defineConfig(({ mode }) => {
+    const base = mode === "production" ? "/savant/" : "/"
+
+    return {
+        root: "./src/docs",
+        base: base,
+        plugins: [tailwindcss()],
+    }
 })
